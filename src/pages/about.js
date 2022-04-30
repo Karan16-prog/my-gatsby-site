fb7aa94643e65b676915d14897a2193d93c38c0e
Karan16-prog <karan.s1606@gmail.com>
2022-04-30 10:37:24 +0530

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+const AboutPage = (props) => {
+  return (
+    <Layout pageTitle="About Page">
+      <p>ABOUT PAGE LOMFSASF.</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
